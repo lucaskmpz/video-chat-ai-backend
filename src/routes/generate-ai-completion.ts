ab9915303e3e25c69fb2d7c9aee44b0244ca1fc7
Lucas <lucasclmaia0@gmail.com>
2023-09-15 22:28:22 -0300

    feat stream

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { createReadStream } from "node:fs"
 import { z } from "zod"
+import { streamToResponse, OpenAIStream } from "ai"
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -32,9 +33,17 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
             messages: [
                 { role: "user", content: promptMessage },
             ],
+            stream: true,
         })
 
-        return response
+        const stream = OpenAIStream(response)
+
+        streamToResponse(stream, reply.raw, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST"
+            },
+        })
 
     })
 }
